22e65187efa3dc9f17f3527760c3590db9afb385
Thies Strothmann <thies.strothmann@rwth-aachen.de>
2018-02-27 21:35:57 +0100

    visitors 2.0

--- src/main/java/tys/frontier/code/visitor/ClassVisitor.java
+++ src/main/java/tys/frontier/code/visitor/ClassVisitor.java
@@ -5,26 +5,27 @@ import tys.frontier.code.FField;
 import tys.frontier.code.FFunction;
 import tys.frontier.code.statement.FStatement;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public interface ClassVisitor<Class, Field, Function, Statement> {
 
-    Class enterClass(FClass clazz);
+    default Class visit(FClass clazz) {
+        return clazz.accept(this);
+    }
+    Statement visit(FStatement statement);
 
-    Field enterField(FField field);
+    //Top down
+    default void enterClass(FClass clazz) {}
+    default void enterField(FField field) {}
+    default void enterFunction(FFunction function) {}
 
-    Function enterFunction(FFunction function);
-
+    //Bottom Up
     default Class exitClass(FClass clazz, List<Field> fields, List<Function> functions) {return null;}
-
     default Field exitField(FField field, Optional<Statement> assign) {return null;}
-
     default Function exitFunction(FFunction function, List<Statement> body) {return null;}
 
-    class Default<Class, Field, Function, Statement, Expression> implements ClassVisitor<Class, Field, Function, Statement> {
-
+    abstract class Default<Class, Field, Function, Statement, Expression> implements ClassVisitor<Class, Field, Function, Statement> {
         protected StatementVisitor<Statement, Expression> stVis;
 
         public Default(StatementVisitor<Statement, Expression> statementVisitor) {
@@ -32,43 +33,8 @@ public interface ClassVisitor<Class, Field, Function, Statement> {
         }
 
         @Override
-        public Class enterClass(FClass clazz) {
-            List<Field> fields = new ArrayList<>(clazz.getFields().size());
-            for (FField f : clazz.getFields().values())
-                fields.add(enterField(f));
-            List<Function> functions = new ArrayList<>(clazz.getFunctions().size());
-            for (FFunction f : clazz.getFunctions().values())
-                functions.add(enterFunction(f));
-            return exitClass(clazz, fields, functions);
-        }
-
-        @Override
-        public Field enterField(FField field) {
-            Optional<Statement> assign = field.getAssignment().map(a ->stVis.enterStatement((a)));
-            return exitField(field, assign);
-        }
-
-        @Override
-        public Function enterFunction(FFunction function) {
-            List<Statement> body = new ArrayList<>(function.getBody().size());
-            for (FStatement s : function.getBody())
-                body.add(stVis.enterStatement(s));
-            return exitFunction(function, body);
-        }
-
-        @Override
-        public Class exitClass(FClass clazz, List<Field> fields, List<Function> functions) {
-            return null;
-        }
-
-        @Override
-        public Field exitField(FField field, Optional<Statement> assign) {
-            return null;
-        }
-
-        @Override
-        public Function exitFunction(FFunction function, List<Statement> body) {
-            return null;
+        public Statement visit(FStatement statement) {
+            return stVis.visit(statement);
         }
     }
 }
