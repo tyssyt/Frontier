22776a413460087b91236885496b46098e691ce9
Thies Strothmann <thies.strothmann@rwth-aachen.de>
2018-04-24 17:14:23 +0200

    updated IO Module

--- src/main/java/tys/frontier/modules/io/IOModule.java
+++ src/main/java/tys/frontier/modules/io/IOModule.java
@@ -1,22 +1,18 @@
 package tys.frontier.modules.io;
 
-import tys.frontier.code.FClass;
 import tys.frontier.code.module.Module;
-
-import java.util.Collections;
-import java.util.List;
 
 public class IOModule extends Module {
 
     public static final IOModule INSTANCE = new IOModule(
             "IO",
             "1.0",
-            null,
-            Collections.singletonList(IOClass.INSTANCE)
+            null
     );
 
-    private IOModule(String name, String version, String subversion_or_versionSuffix, List<FClass> exportedClasses) {
-        super(name, version, subversion_or_versionSuffix, exportedClasses);
+    private IOModule(String name, String version, String subversion_or_versionSuffix) {
+        super(name, version, subversion_or_versionSuffix);
+        addExportedClass(IOClass.INSTANCE);
     }
 
 }
