28279544c7fd3b377519a2c5a3b97c6aac9db203
Thies Strothmann <thies.strothmann@rwth-aachen.de>
2018-04-27 10:25:51 +0200

    arrays have fields, so this check is bad

--- src/main/java/tys/frontier/code/predefinedClasses/FPredefinedClass.java
+++ src/main/java/tys/frontier/code/predefinedClasses/FPredefinedClass.java
@@ -1,7 +1,6 @@
 package tys.frontier.code.predefinedClasses;
 
 import tys.frontier.code.FClass;
-import tys.frontier.code.FField;
 import tys.frontier.code.FVisibilityModifier;
 import tys.frontier.code.identifier.FClassIdentifier;
 
@@ -9,10 +8,5 @@ public abstract class FPredefinedClass extends FClass {
 
     public FPredefinedClass(FClassIdentifier identifier) {
         super(identifier, FVisibilityModifier.EXPORT, false);
-    }
-
-    @Override
-    public void addField(FField field) {
-        throw new RuntimeException("somthing went terribly wrong");
     }
 }
