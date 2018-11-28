76238ada4a366cf74876fc0637c810f5f6422227
Thies Strothmann <thies.strothmann@rwth-aachen.de>
2018-11-28 17:43:34 +0100

    don't remove anything in predefined classes

--- src/main/java/tys/frontier/code/predefinedClasses/FPredefinedClass.java
+++ src/main/java/tys/frontier/code/predefinedClasses/FPredefinedClass.java
@@ -3,10 +3,14 @@ package tys.frontier.code.predefinedClasses;
 import tys.frontier.code.FClass;
 import tys.frontier.code.FVisibilityModifier;
 import tys.frontier.code.identifier.FTypeIdentifier;
+import tys.frontier.passes.analysis.reachability.Reachability;
 
 public abstract class FPredefinedClass extends FClass {
 
     public FPredefinedClass(FTypeIdentifier identifier) {
         super(identifier, FVisibilityModifier.EXPORT);
     }
+
+    @Override
+    public void removeUnreachable(Reachability.ReachableClass reachable) {}
 }
